23faa5fbbee5638a1c4e8a93d3306830172b59ad
jcislemadev-coder <jcislemadev@gmail.com>
2026-04-25 19:54:33 -0500

    inclusion de funciones basicas para el funcionamiento

--- funciones.js
+++ funciones.js
@@ -1,1 +1,47 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible (ingresos,egresos){
+    let saldo = ingresos - egresos
+    if (saldo < 0){
+        saldo = 0;
+    }
+    return(saldo)
+}
+
+function calcularCapacidadPago (montoDisponible){
+    let capacidad = montoDisponible * 0.50;
+    return (capacidad)
+}
+
+function calcularInteresSimple (monto,tasa,plazoAnios){
+    let interes = plazoAnios * monto * (tasa/100);
+    return interes;
+}
+
+function calcularTotalaPagar (monto , tasa, plazoAnios) {
+    let interes = calcularInteresSimple (monto,tasa,plazoAnios);
+    let total = interes + monto + 100;
+
+    return total;
+}
+
+function calcularCuotaMensual (monto,tasa,plazoAnios){
+    let total = calcularTotalaPagar(monto,tasa,plazoAnios)
+    let cuota = total / (plazoAnios*12)
+    return cuota;
+
+}
+
+function analizarCredito (montoDisponible,monto,tasa,plazaAnios){
+    let status ;
+    let capacidad = calcularCapacidadPago(montoDisponible);
+    let cuota = calcularCuotaMensual(monto,tasa,plazaAnios);
+
+    if (capacidad > cuota){
+         status = true;
+    }
+    else{
+        status = false;
+    }
+
+    return status;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,53 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    let cmpingresos = document.getElementById("txtIngresos");
+    let ingresossrc = cmpingresos.value;
+    let ingresosFloat = parseFloat (ingresossrc);
+
+    let cmpegresos = document.getElementById("txtEgresos");
+    let egresossrc = cmpegresos.value;
+    let egresosFloat = parseFloat (egresossrc);
+
+    let saldof = calcularDisponible(ingresosFloat , egresosFloat);
+
+    let disponiblespn = document.getElementById("spnDisponible")
+    disponiblespn.textContent = saldof;
+
+    let capacidadpago = calcularCapacidadPago(saldof);
+
+    let spncapacidad = document.getElementById("spnCapacidadPago")
+    spncapacidad.textContent = capacidadpago;
+
+    let cmpmonto = document.getElementById("txtMonto");
+    let cmpplazo = document.getElementById("txtPlazo");
+    let cmptasa = document.getElementById("txtTasaInteres");
+
+    let montosrc = cmpmonto.value;
+    let montoE = parseInt(montosrc);
+
+    let plazosrc = cmpplazo.value;
+    let plazoE = parseInt(plazosrc);
+
+    let tasasrc = cmptasa.value;
+    let tasaE = parseInt(tasasrc);
+
+    let interesc = calcularInteresSimple(montoE , tasaE , plazoE);
+
+    let spninteres = document.getElementById("spnInteresPagar");
+    spninteres.textContent = interesc;
+
+    let total = calcularTotalaPagar(montoE,tasaE,plazoE);
+
+    let totalprestamosrc= document.getElementById("spnTotalPrestamo")
+    totalprestamosrc.textContent = total;
+
+    let cuota =  calcularCuotaMensual(montoE, tasaE, plazoE);
+
+    let cuotasrc = document.getElementById("spnCuotaMensual");
+    cuotasrc.textContent = cuota;
+
+    let analizar = analizarCredito(saldof , montoE, tasaE, plazoE)
+    let status= document.getElementById("spnEstadoCredito");
+
+    
+}
